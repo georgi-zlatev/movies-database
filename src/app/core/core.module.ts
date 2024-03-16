7277ae3bdf3875f48b651721537bb30f8eb07cb4
Georgi Zlatev <georgi.n.zlatev@gmail.com>
2024-03-16 15:02:44 +0200

    Created separete mini nav bar component - with headline and login register buttons

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MiniNavBarComponent } from './mini-nav-bar/mini-nav-bar.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MiniNavBarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MiniNavBarComponent,
   ]
 })
 export class CoreModule { }
---
+++ src/app/core/mini-nav-bar/mini-nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-nav-bar',
+  templateUrl: './mini-nav-bar.component.html',
+  styleUrls: ['./mini-nav-bar.component.css']
+})
+export class MiniNavBarComponent {
+
+}
