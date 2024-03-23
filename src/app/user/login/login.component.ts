58ac1cd654053a996cbe811f9869fa9b777f4cad
Georgi Zlatev <georgi.n.zlatev@gmail.com>
2024-03-23 17:35:44 +0200

    small imporovement on login component

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserServiceService } from '../user-service.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+constructor (private userService: UserServiceService, private router: Router){}
+login(form:NgForm){
+  if (form.invalid) {
+    return
+  }
+  console.log(form.value);
+  
+  this.userService.login();
+  this.router.navigate(['/home'])
+}
 }
