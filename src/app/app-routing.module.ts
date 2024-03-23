ff6df15e62ef1916bec7bf7ba91aa2e52b18c10a
Georgi Zlatev <georgi.n.zlatev@gmail.com>
2024-03-23 17:02:53 +0200

    created add movie form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { AwardsListComponent } from './awards-list/awards-list.component';
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
 import { MovieDetailsComponent } from './movies-list/movie-details/movie-details.component';
+import { AddMovieComponent } from './movies/add-movie/add-movie.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'movies', component: MoviesListComponent },
   { path: 'awards', component: AwardsListComponent },
   { path: 'movie/:id', component: MovieDetailsComponent },
+  { path: 'add', component: AddMovieComponent },
   { path: '**', redirectTo: '/404' },
   { path: '404', component: ErrorComponent }
 
